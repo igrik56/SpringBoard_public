6cdebfbf6f2898aa77f7ee9ea9975d6e0e22e4c8
Halim Tannous <48407395+halimt92@users.noreply.github.com>
2023-02-20 23:31:28 +0100

    fix: flask blogly
    this commit fixes multiple issues:
    - backreferencing
    - property on date of post model
    - updated_at on post model
    - restful routes

--- flask-blogly/app.py
+++ flask-blogly/app.py
@@ -9,11 +9,11 @@ time_now = datetime.now()
 
 app = Flask(__name__)
 
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = True
-app.config['SECRET_KEY'] = 'PassSecretWord'
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///blogly"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+app.config["SECRET_KEY"] = "PassSecretWord"
 
 toolbar = DebugToolbarExtension(app)
 
@@ -21,214 +21,224 @@ app.app_context().push()
 connect_db(app)
 db.create_all()
 
-@app.route('/')
-def home_page():
-    return redirect ('/users')
 
-@app.route('/users')
+@app.route("/")
+def home_page():
+    return redirect("/users")
+
+
+@app.route("/users")
 def users():
-    """ Home page. Shows list of users"""
+    """Home page. Shows list of users"""
     users = User.query.all()
 
-    return render_template('index.html', users = users)
+    return render_template("index.html", users=users)
 
-@app.route('/users', methods = ["POST"])
+
+@app.route("/users", methods=["POST"])
 def user_create():
-    '''Creating new users'''
+    """Creating new users"""
 
-    first_name = request.form['first_name']
-    last_name = request.form['last_name']
-    img_url = request.form['img_url']
+    first_name = request.form["first_name"]
+    last_name = request.form["last_name"]
+    img_url = request.form["img_url"]
     img_url = img_url if img_url else None
 
-    new_user = User(first_name = first_name, last_name = last_name, img_url = img_url)
+    new_user = User(first_name=first_name, last_name=last_name, img_url=img_url)
     db.session.add(new_user)
     db.session.commit()
 
-    return redirect(f'/users/{new_user.id}')
+    return redirect(f"/users/{new_user.id}")
 
-@app.route('/users/<int:user_id>')
+
+@app.route("/users/<int:user_id>")
 def user_details(user_id):
-    '''Show user's details'''
+    """Show user's details"""
 
     user = User.query.get_or_404(user_id)
 
-    return render_template('user_details.html', user= user)
+    return render_template("user_details.html", user=user)
 
-@app.route('/users/<int:user_id>/edit')
+
+@app.route("/users/<int:user_id>/edit")
 def user_edit(user_id):
-    '''Edit page for users'''
+    """Edit page for users"""
 
     user = User.query.get_or_404(user_id)
 
-    return render_template('edit.html', user = user)
+    return render_template("edit.html", user=user)
 
-@app.route('/users/<int:user_id>/edit', methods = ['POST'])
+
+@app.route("/users/<int:user_id>", methods=["PUT"])
 def user_update(user_id):
-    '''Post user's changes to database'''
+    """Post user's changes to database"""
 
     user = User.query.get_or_404(user_id)
 
-    user.first_name = request.form['first_name']
-    user.last_name = request.form['last_name']
-    user.img_url = request.form['img_url']
+    user.first_name = request.form["first_name"]
+    user.last_name = request.form["last_name"]
+    user.img_url = request.form["img_url"]
 
     db.session.add(user)
     db.session.commit()
 
-    return redirect('/users')
+    return redirect("/users")
 
-@app.route('/users/<int:user_id>/delete')
+
+@app.route("/users/<int:user_id>/delete")
 def user_delete_confirm(user_id):
-    '''Delete user confirmation'''
+    """Delete user confirmation"""
 
     user = User.query.get_or_404(user_id)
 
-    return render_template('delete_confirmation.html', user = user)
+    return render_template("delete_confirmation.html", user=user)
 
-@app.route('/users/<int:user_id>/delete', methods = ['POST'])
+
+@app.route("/users/<int:user_id>", methods=["DELETE"])
 def user_delete(user_id):
-    '''Delete user in DB'''
+    """Delete user in DB"""
 
     user = User.query.get_or_404(user_id)
     db.session.delete(user)
     db.session.commit()
-    return redirect('/users')
+    return redirect("/users")
 
-@app.route('/users/<int:user_id>/post')
+
+@app.route("/users/<int:user_id>/posts")
 def post_new_form(user_id):
-
     user = User.query.get_or_404(user_id)
     tags = Tag.query.all()
 
-    return render_template('post_new.html', user = user, tags = tags)
+    return render_template("post_new.html", user=user, tags=tags)
 
-@app.route('/users/<int:user_id>/post', methods = ['POST'])
+
+@app.route("/users/<int:user_id>/posts", methods=["POST"])
 def post_new_add(user_id):
-    
     user = User.query.get_or_404(user_id)
 
-    title = request.form['title']
-    content = request.form['content']
-    tags_ids = [ int(t) for t in request.form.getlist('tags')]
+    title = request.form["title"]
+    content = request.form["content"]
+    tags_ids = [int(t) for t in request.form.getlist("tags")]
     tags = Tag.query.filter(Tag.id.in_(tags_ids)).all()
 
-    new_post = Post(title = title, content = content, user_id = user.id, tags = tags)
+    new_post = Post(title=title, content=content, user_id=user.id, tags=tags)
 
     db.session.add(new_post)
     db.session.commit()
 
-    return redirect(f'/users/{user_id}')
+    return redirect(f"/users/{user_id}")
 
-@app.route('/users/<int:user_id>/<int:post_id>')
+
+@app.route("/users/<int:user_id>/posts/<int:post_id>")
 def user_post(user_id, post_id):
-
     user = User.query.get_or_404(user_id)
     post = Post.query.get_or_404(post_id)
 
     tags = Tag.query.all()
 
-    return render_template('post.html', user = user, post = post, tags =tags)
+    return render_template("post.html", user=user, post=post, tags=tags)
 
-@app.route('/users/<int:user_id>/<int:post_id>/edit')
+
+@app.route("/users/<int:user_id>/posts/<int:post_id>/edit")
 def post_edit(user_id, post_id):
-
     user = User.query.get_or_404(user_id)
     post = Post.query.get_or_404(post_id)
 
     tags = Tag.query.all()
 
-    return render_template('post_edit.html', user = user, post = post, tags = tags)
+    return render_template("post_edit.html", user=user, post=post, tags=tags)
 
-@app.route('/users/<int:user_id>/<int:post_id>/edit', methods = ['POST'])
+
+@app.route("/users/<int:user_id>/posts/<int:post_id>", methods=["PUT"])
 def post_update(user_id, post_id):
-    '''update post's changes to database'''
+    """update post's changes to database"""
 
     # user = User.query.get_or_404(user_id)
     post = Post.query.get_or_404(post_id)
 
-    post.title = request.form['title']
-    post.content = request.form['content']
-    tags_ids = [ int(t) for t in request.form.getlist('tags')]
+    post.title = request.form["title"]
+    post.content = request.form["content"]
+    tags_ids = [int(t) for t in request.form.getlist("tags")]
     post.tags = Tag.query.filter(Tag.id.in_(tags_ids)).all()
-    
-    # post.date = time_now.strftrime("%m/%d/%Y, %H:%M:%S") - broke at the moment T_T
 
+    # post.date = time_now.strftrime("%m/%d/%Y, %H:%M:%S") - broke at the moment T_T
+    post.updated_at = datetime.now()
     db.session.add(post)
     db.session.commit()
 
-    return redirect(f'/users/{user_id}/{post_id}')
+    return redirect(f"/users/{user_id}/{post_id}")
 
-@app.route('/users/<int:user_id>/<int:post_id>/delete', methods = ['POST'])
+
+@app.route("/users/<int:user_id>/posts/<int:post_id>", methods=["DELETE"])
 def post_delete(user_id, post_id):
-
     # user = User.query.get_or_404(user_id)
     post = Post.query.get_or_404(post_id)
 
     db.session.delete(post)
     db.session.commit()
 
-    return redirect(f'/users/{user_id}')
+    return redirect(f"/users/{user_id}")
 
-@app.route('/tags')
+
+@app.route("/tags")
 def tags():
     tags = Tag.query.all()
 
-    return render_template('tags.html', tags=tags)
+    return render_template("tags.html", tags=tags)
 
-@app.route('/tags/<int:tag_id>')
+
+@app.route("/tags/<int:tag_id>")
 def tag(tag_id):
-
     tag = Tag.query.get_or_404(tag_id)
 
-    return render_template('/tag_details.html', tag=tag)
+    return render_template("/tag_details.html", tag=tag)
 
-@app.route('/tags/new')
+
+@app.route("/tags/new")
 def tags_new():
-
     tags = Tag.query.all()
-        
-    return render_template('tag_new.html', tags=tags)
 
-@app.route('/tags/new', methods = ['POST'])
+    return render_template("tag_new.html", tags=tags)
+
+
+@app.route("/tags", methods=["POST"])
 def tags_new_add():
+    name = request.form["tag"]
 
-    name = request.form['tag']
-
-    new_tag = Tag(name = name)
+    new_tag = Tag(name=name)
 
     db.session.add(new_tag)
     db.session.commit()
 
-    flash('Your tag has been added', 'Notification')
+    flash("Your tag has been added", "Notification")
 
-    return redirect ('/')
+    return redirect("/")
 
-@app.route('/tags/<int:tag_id>/edit')
+
+@app.route("/tags/<int:tag_id>/edit")
 def tags_edit(tag_id):
-    
     tag = Tag.query.get_or_404(tag_id)
 
-    return render_template('tag_edit.html', tag=tag)
+    return render_template("tag_edit.html", tag=tag)
 
-@app.route('/tags/<int:tag_id>/edit', methods = ['POST'])
+
+@app.route("/tags/<int:tag_id>", methods=["PUT"])
 def tags_edit_add(tag_id):
-
     tag = Tag.query.get_or_404(tag_id)
 
-    tag.name = request.form['name']
+    tag.name = request.form["name"]
 
     db.session.add(tag)
     db.session.commit()
 
-    return redirect('/tags')
+    return redirect("/tags")
 
-@app.route('/tags/<int:tag_id>/delete', methods =['POST'])
+
+@app.route("/tags/<int:tag_id>", methods=["DELETE"])
 def tags_delete(tag_id):
-
     tag = Tag.query.get_or_404(tag_id)
 
     db.session.delete(tag)
     db.session.commit()
 
-    return redirect('/tags')
+    return redirect("/tags")
--- flask-blogly/models.py
+++ flask-blogly/models.py
@@ -1,70 +1,80 @@
 """Models for Blogly."""
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from sqlalchemy.sql import func
 
+db = SQLAlchemy()
 
-
-db=SQLAlchemy()
 
 def connect_db(app):
     db.app = app
     db.init_app(app)
 
+
 class User(db.Model):
-    '''Model for Users'''
-    
-    __tablename__ = 'users'
+    """Model for Users"""
 
-    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
-    
-    first_name = db.Column(db.String(25), nullable = False)
-    last_name = db.Column(db.String(25), nullable = False)    
-    img_url = db.Column(db.String(200), nullable = False, default = 'static/def_user_img.jpg')
+    __tablename__ = "users"
 
-    post = db.relationship('Post', backref = 'users', cascade = 'all, delete-orphan')   #establish relationship to get Post data via User
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 
+    first_name = db.Column(db.String(25), nullable=False)
+    last_name = db.Column(db.String(25), nullable=False)
+    img_url = db.Column(
+        db.String(200), nullable=False, default="static/def_user_img.jpg"
+    )
+    posts = db.relationship("Post", backref="creator")
 
     def __repr__(self):
-        return f'<User {self.first_name}, {self.last_name}, {self.img_url}'
+        return f"<User {self.first_name}, {self.last_name}, {self.img_url}"
 
 
-    
 class Post(db.Model):
-    '''Model for Posts'''
-    
-    __tablename__ = 'posts'
+    """Model for Posts"""
 
-    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
-    title = db.Column(db.String(70), nullable = False, unique = True)
-    content = db.Column(db.String(2000), nullable = False, unique = True)
-    date = db.Column(db.String(25), nullable = False, default = datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    __tablename__ = "posts"
 
-    creator = db.relationship('User', backref = 'posts')
-    tag = db.relationship('Tag', secondary = 'posts_tags', backref = 'posts')
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(70), nullable=False, unique=True)
+    content = db.Column(db.String(2000), nullable=False, unique=True)
+    created_at = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=func.Now(),
+    )
+    updated_at = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=func.Now(),
+    )
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    tags = db.relationship("Tag", secondary="posts_tags", backref="posts")
 
     def __repr__(self):
-        return f'<Post {self.title}, {self.content}, {self.user_id}'
-    
+        return f"<Post {self.title}, {self.content}, {self.user_id}"
+
+    @property
+    def str_updated_date(self):
+        return self.updated_at.strftime("%m/%d/%Y, %H:%M:%S")
+
 
 class Tag(db.Model):
-    '''Model for Tags'''
+    """Model for Tags"""
 
-    __tablename__ = 'tags'
+    __tablename__ = "tags"
 
-    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
-    name = db.Column(db.String(15), nullable = False, unique = True)
-
-    post = db.relationship('Post', secondary= 'posts_tags', backref = 'tags')
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(15), nullable=False, unique=True)
 
     def __repr__(self):
-        return f'<Tag: {self.id}, {self.name}'
+        return f"<Tag: {self.id}, {self.name}"
+
 
 class PostTag(db.Model):
-    '''Model for M2M table for post and tag'''
+    """Model for M2M table for post and tag"""
 
-    __tablename__ = 'posts_tags'
+    __tablename__ = "posts_tags"
 
-    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), primary_key =True)
-    tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'), primary_key =True)
-
+    post_id = db.Column(db.Integer, db.ForeignKey("posts.id"), primary_key=True)
+    tag_id = db.Column(db.Integer, db.ForeignKey("tags.id"), primary_key=True)

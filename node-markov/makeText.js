36159c26f3b108d625006fa57f18a79159549de7
igrik56 <igrik1991@gmail.com>
2023-03-25 20:25:09 -0400

    31.5_done

--- node-markov/makeText.js
+++ node-markov/makeText.js
@@ -1,2 +1,46 @@
 /** Command-line tool to generate Markov text. */
 
+const fs = require('fs')
+const axios = require('axios')
+const process = require('process')
+const markov = require('./markov')
+
+
+function createText(text){
+    let mm = new markov.MarkovMachine(text)
+    console.log(mm.makeText())
+}
+
+function makeText(path){
+    fs.readFile(path, 'utf-8', function res(err, data){
+        if(err){
+            console.error(`Can't read file: ${path}: ${err}`)
+            process.exit(1)
+        }
+        else createText(data)
+    })
+}
+
+async function makeUrlText(url){
+    let res
+
+    try{
+        res = await axios.get(url)
+    }
+    catch (err){
+        console.error(`Can't read url: ${url}: ${err}`)
+        process.exit(1)
+    }
+    createText(res.data)
+}
+
+let [method, path] = process.argv.slice(2)
+
+if (method === `file`){
+    makeText(path)
+}
+else if (method === `url`) makeUrlText(path)
+else {
+    console.error(`Invalid method: ${method}`)
+    process.exit(1)
+}
--- node-markov/markov.js
+++ node-markov/markov.js
@@ -18,12 +18,40 @@ class MarkovMachine {
 
   makeChains() {
     // TODO
+    let chains = new Map()
+
+    for(let i=0; i<this.words.length; i+=1){
+      let word = this.words[i]
+      let nextWord = this.words[i+1] || null
+
+      if(chains.has(word)) chains.get(word).push(nextWord);
+      else chains.set(word, [nextWord])
+    }
+    this.chains = chains
+    // console.log (this.chains)
   }
 
-
+static choice(keys){
+  return keys[Math.floor(Math.random()* keys.length)]
+}
   /** return random text from chains */
 
   makeText(numWords = 100) {
     // TODO
+    let keys = Array.from(this.chains.keys())
+    let key = MarkovMachine.choice(keys)
+    let output = []
+
+  while (output.length < numWords && key !== null){
+    output.push(key)
+    key = MarkovMachine.choice(this.chains.get(key))
   }
+  return output.join(` `)
 }
+}
+
+module.exports = {
+  MarkovMachine
+}
+
+// let mm = new MarkovMachine(`the cat in the hat`)
